ed2a76cc0075fa5cb2d91b0d4851cda0ea35b712
Andrea Landeros <60786005+Andirux@users.noreply.github.com>
2024-03-15 17:10:55 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2500)
-  .typeString('Diseño y desarrollo sitios web')
+  .typeString('Diseño y desarrollo sitios web creativos')
   .pauseFor(200)
   .deleteChars(10)
   .start();
